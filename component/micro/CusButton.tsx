a8758d7c10f6eafee6fc7965ea1789df4e7e3383
sridhar-mani <2019309038@student.annauniv.edu>
2025-04-27 16:33:02 +0530

    refactor: enhance CusButton with pressed state and style adjustments

--- component/micro/CusButton.tsx
+++ component/micro/CusButton.tsx
@@ -2,15 +2,25 @@ import {  Text, Pressable } from 'react-native';
 import React from 'react';
 import tw from 'twrnc';
 import { CusButtonProps } from '../../types';
-
+import Animated,{useAnimatedStyle} from 'react-native-reanimated';
 
 const CusButton = ({title,onPress,variant='primary'}:CusButtonProps) => {
-  return (
-    <Pressable onPress={onPress} style={({pressed})=>
-        tw.style(`px-4 py-2 hover:shadow-md active:scale-0.9 rounded-md ${variant === 'primary' ? 'bg-blue-500' : 'bg-gray-500'}`,pressed && 'opacity-70 transition-all duration-300 scale-95')
 
-    }>
-      <Text style={tw`text-white text-base`}>{title}</Text>
+    const [pressed, setPressed] = React.useState(false);
+    // const animatedStyle = useAnimatedStyle(()=>({
+    //     transform: [{ scale: pressed ? 0.95 : 1 }],
+    // }))
+
+  return (
+    <Pressable onPressIn={()=>setPressed(true)} onPressOut={()=>setPressed(false)} onPress={onPress}>
+      {/* <Animated.View style={[
+          tw.style(
+            `px-4 py-2 rounded-lg ${variant === 'primary' ? 'bg-blue-500' : 'bg-gray-500'}`
+          ),
+          animatedStyle,
+      ]}>  */}
+      <Text style={tw`text-white text-center font-semibold`}>{title}</Text>
+      {/* </Animated.View> */}
     </Pressable>
   )
 }
