b06445595df044d731b7fc623680deb8acb24f19
Sridhar.Mani <sridhar.mani@novacastindia.com>
2025-05-15 17:28:55 +0530

    feat: Update package.json dependencies and add CameraScreen
    - Updated dependencies in package.json for better performance and compatibility.
    - Added CameraScreen component for capturing and processing images using the camera.
    - Enhanced HistoryScreen with additional filtering and transaction management features.
    - Introduced Camera functionality with text recognition capabilities.

---
+++ src/screens/CameraScreen.tsx
@@ -0,0 +1,55 @@
+import React, {useEffect} from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {
+  Camera,
+  useCameraDevice,
+  useCameraDevices,
+  useFrameProcessor,
+} from 'react-native-vision-camera';
+import {runOnJS} from 'react-native-reanimated';
+import tailwind from 'twrnc';
+import TextRecognition from '@react-native-ml-kit/text-recognition';
+import {Alert} from 'react-native';
+
+const CameraScreen = () => {
+  const [hasPermission, setHasPermission] = React.useState(false);
+  const devices = useCameraDevice('back');
+  console.log(devices);
+
+  useEffect(() => {
+    const requestCameraPerm = async () => {
+      const state = await Camera.requestCameraPermission();
+      if (state === 'denied') {
+        Alert.alert(
+          'Camera Permission',
+          'Camera access is required to scan receipts.',
+        );
+      }
+      setHasPermission(state === 'granted');
+    };
+    requestCameraPerm();
+  }, []);
+
+  const frameProcessor = useFrameProcessor(frame => {
+    'worklet';
+    runOnJS(processFrame)(frame);
+  }, []);
+
+  const processFrame = async frame => {
+    try {
+      const {blocks} = await TextRecognition.recognize(frame);
+      const lines = blocks.flatMap(b => b.lines.map(l => l.text));
+      console.log('Detected lines:', lines);
+    } catch (e) {
+      console.warn('Error processing frame: ', e);
+    }
+  };
+
+  return (
+    <SafeAreaView>
+      <Camera device={devices} isActive={true} style={{flex: 1}}></Camera>
+    </SafeAreaView>
+  );
+};
+
+export default CameraScreen;
