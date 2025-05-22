87f9a6ff0e8e49494fef3c4817729c5473810118
Sridhar.Mani <sridhar.mani@novacastindia.com>
2025-05-22 15:59:47 +0530

    Revert "feat: Update package.json dependencies and add CameraScreen"
    This reverts commit b06445595df044d731b7fc623680deb8acb24f19.

--- src/constants/componenetList.ts
+++ src/constants/componenetList.ts
@@ -2,7 +2,6 @@ import HomeScreen from '../screens/HomeScreen';
 import HistoryScreen from '../screens/HistoryScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 import AnalyticsScreen from '../screens/AnalyticsScreen';
-import CameraScreen from '../screens/CameraScreen';
 
 const navigations = [
   {
@@ -33,13 +32,6 @@ const navigations = [
   //   unfocusedIcon: 'settings-outline',
   //   component: SettingsScreen,
   // },
-  {
-    key: 'camera',
-    title: 'Camera',
-    focusedIcon: 'camera',
-    unfocusedIcon: 'camera-outline',
-    component: CameraScreen,
-  },
 ];
 
 export {navigations};
